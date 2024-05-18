c061b83455c7d09529e618746268a0f93f54ec8c
scuba75 <scuba75@gmail.com>
2024-05-18 07:43:12 -0500

    update

--- src/sync/checkLeagues.js
+++ src/sync/checkLeagues.js
@@ -4,7 +4,7 @@ const scanLeague = require('./scanLeague')
 module.exports = async(gaEvent = {})=>{
   if(!gaEvent?.leagues) return
   for(let i in gaEvent.leagues){
-    if(!gaEvent.leagues) continue
+    if(!gaEvent.leagues[i]) continue
     await scanLeague({ ...JSON.parse(JSON.stringify(gaEvent)),...gaEvent.leagues[i] })
   }
 }
