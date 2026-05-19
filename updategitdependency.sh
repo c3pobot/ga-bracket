ab2b17d87fed73c830cb5607be5cea3cdf16896c
scuba75 <scuba75@gmail.com>
2026-05-19 12:56:34 -0500

    update

--- updategitdependency.sh
+++ updategitdependency.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo updating mongoclient
-npm i --package-lock-only github:c3pobot/mongoclient
-echo updating logger
-npm i --package-lock-only github:c3pobot/logger
+echo updating mongo-cache
+npm i --package-lock-only github:c3pobot/mongo-cache
